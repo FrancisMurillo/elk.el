9aa51cd36407aba0f74c49d46c813751de492c94
Francis Murillo <francisavmurillo@gmail.com>
2016-08-23 22:13:01 +0800

    /-consume-whitespace test added

---
+++ test/elk-consume-test.el
@@ -0,0 +1,88 @@
+;;; elk-consume-test.el --- elk
+;;
+;; Filename: elk-consume-test.el
+;; Description:
+;; Author: Francis Murillo
+;; Maintainer:
+;; Created: Tue Aug 23 21:51:24 2016 (+0800)
+;; Version:
+;; Package-Requires: ()
+;; Last-Updated:
+;;           By:
+;;     Update #: 0
+;; URL:
+;; Doc URL:
+;; Keywords:
+;; Compatibility:
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Commentary:
+;;
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Change Log:
+;;
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or (at
+;; your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful, but
+;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <http://www.gnu.org/licenses/>.
+;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;
+;;; Code:
+
+
+(require 's)
+
+
+;;* consume-whitespace
+(ert-deftest elk--consume-whitespace-test/base ()
+  (let* ((source-code " \n\t  ")
+         (source-stream (elk--started-stream source-code))
+         (token (elk--consume-whitespace source-stream)))
+    (should (eq 'whitespace
+                (plist-get token :type)))
+    (should (null (plist-get token :tokens)))
+    (should (= 0
+               (plist-get token :start-pos)))
+    (should (= -1
+               (plist-get token :end-pos)))))
+
+(ert-deftest elk--consume-whitespace-test/partial ()
+  (let* ((source-code " \n|\t  ")
+         (source-stream (elk--started-stream source-code))
+         (token (elk--consume-whitespace source-stream)))
+    (should (eq 'whitespace
+                (plist-get token :type)))
+    (should (null (plist-get token :tokens)))
+    (should (= 0
+               (plist-get token :start-pos)))
+    (should (= (s-index-of "|" source-code)
+               (plist-get token :end-pos)))))
+
+(ert-deftest elk--consume-whitespace-test/nil ()
+  (let* ((source-code "STOP")
+         (source-stream (elk--started-stream source-code))
+         (token (elk--consume-whitespace source-stream)))
+    (should (null token))))
+
+
+
+(provide 'elk-consume-test)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;; elk-consume-test.el ends here
